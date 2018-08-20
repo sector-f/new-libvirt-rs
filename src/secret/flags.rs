cba73d6c90effa72f27c82df7c031be4bb777568
kirjavascript <snkenjoi@gmail.com>
2018-08-20 23:01:44 +0100

    OK CHANGED FLAGS

--- src/secret/flags.rs
+++ src/secret/flags.rs
@@ -9,8 +9,8 @@ bitflags! {
         const VIR_SECRET_USAGE_TYPE_NONE = 0;
         const VIR_SECRET_USAGE_TYPE_VOLUME = 1;
         const VIR_SECRET_USAGE_TYPE_CEPH = 2;
-        const VIR_SECRET_USAGE_TYPE_ISCSI = 4;
-        const VIR_SECRET_USAGE_TYPE_TLS = 8;
-        const VIR_SECRET_USAGE_TYPE_LAST = 16;
+        const VIR_SECRET_USAGE_TYPE_ISCSI = 3;
+        const VIR_SECRET_USAGE_TYPE_TLS = 4;
+        const VIR_SECRET_USAGE_TYPE_LAST = 5;
     }
 }
